e958a8daaf3613edd4966dfb2d5c5817c0e71a1d
PabloHermun <pablo_hermun@ciencias.unam.mx>
2024-01-27 22:13:31 -0600

    Final version. Working and validated with check50 and style50

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -24,10 +24,11 @@ def player(board):
     In the initial game state, X gets the first move. 
     Subsequently, the player alternates with each additional move.
     """
-    if board_sum(board)%2 == 0:
+    if board_sum(board) % 2 == 0:
         return X
     else:
         return O
+
 
 def board_sum(board):
     """
@@ -40,6 +41,7 @@ def board_sum(board):
                 board_sum += 1
     return board_sum
 
+
 def actions(board):
     """
     Returns set of all possible actions (i, j) available (EMPTY tiles) on the board.
@@ -48,7 +50,7 @@ def actions(board):
     for i in range(3):
         for j in range(3):
             if board[i][j] == EMPTY:
-                allowed_actions.add((i,j))
+                allowed_actions.add((i, j))
     return allowed_actions
 
 
@@ -57,21 +59,19 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
     new_board = [[EMPTY, EMPTY, EMPTY],
-                [EMPTY, EMPTY, EMPTY],
-                [EMPTY, EMPTY, EMPTY]]
+                 [EMPTY, EMPTY, EMPTY],
+                 [EMPTY, EMPTY, EMPTY]]
     # Copy the input board and add the action according to current player
     if action in actions(board):
         for i in range(3):
             for j in range(3):
-                if (i,j) == action:
+                if (i, j) == action:
                     new_board[i][j] = player(board)
                 else:
                     new_board[i][j] = board[i][j]
         return new_board
     else:
         raise ValueError
-
-    raise NotImplementedError
 
 
 def winner(board):
@@ -83,18 +83,18 @@ def winner(board):
     for i in range(3):
         symbol = board[i][0]
         if board[i][1] == symbol == board[i][2] != EMPTY:
-                return symbol
+            return symbol
     # Check columns
     for j in range(3):
         symbol = board[0][j]
         if board[1][j] == symbol == board[2][j] != EMPTY:
-                return symbol
+            return symbol
 
     # Check diagonals
     if board[0][0] == board[1][1] == board[2][2] != EMPTY:
         return board[0][0]
 
-    if board[2][0] == board[1][2] == board[0][2] != EMPTY:
+    if board[2][0] == board[1][1] == board[0][2] != EMPTY:
         return board[0][2]
 
     return None
@@ -130,21 +130,25 @@ def utility(board):
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
+    If the board is a terminal board, returns None.
     Process is actually run in minimax_wrapped(), and only the output ACTION
-    is returned to the rest of the program
+    is returned to the rest of the program.
     """
+    if terminal(board):
+        return None
     score, optimal_action = minimax_wrapped(board)
     return optimal_action
 
+
 def minimax_wrapped(board):
     """
-    Minimax will never receive a terminal board directly, only recursively (BASE CASE).
-    Returns both the utility and the action associated
+    This function never receives a terminal board directly, only recursively (BASE CASE).
+    Returns both the utility and the action associated.style
     """
-    #TODO: add inline comments and debug (not recognizing one of the diagonals as winning)
+    # Define a dict to store the utility of each possible action
     utilities = {}
     for action in actions(board):
-        scenario = result(board,action)
+        scenario = result(board, action)
 
         if terminal(scenario): 
             return utility(scenario), action
@@ -158,5 +162,3 @@ def minimax_wrapped(board):
         score = min(utilities.values())
         return score, min(utilities, key=utilities.get)
             
-    raise NotImplementedError
-
